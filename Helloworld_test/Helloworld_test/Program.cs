623d4a0f13d14ca6c75145ca9497d1ea7567df50
Sandhya Manheri <smanheri@illumina.com>
2016-08-25 14:54:27 -0700

    Bank Application

---
+++ Helloworld_test/Helloworld_test/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helloworld_test
+{
+    // method under test
+    public void Debit(double amount)
+    {
+        if (amount > m_balance)
+        {
+            throw new ArgumentOutOfRangeException("amount");
+        }
+        if (amount < 0)
+        {
+            throw new ArgumentOutOfRangeException("amount");
+        }
+        m_balance += amount;
+    }
+}
